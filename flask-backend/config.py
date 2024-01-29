95bedb3958ae395abb7ee65cf39576d31b284aa9
JoDsnr <jodesnoyer@gmail.com>
2024-01-29 20:12:11 +0100

    updated config

--- flask-backend/config.py
+++ flask-backend/config.py
@@ -11,13 +11,9 @@ class ApplicationConfig:
     SESSION_COOKIE_NAME = 'Spotify Cookie'
     AUTH_URL = "https://accounts.spotify.com/authorize"
     TOKEN_URL = "https://accounts.spotify.com/api/token"
-<<<<<<< HEAD
     API_BASE_URL = "https://api.spotify.com/v1"
 
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = r"sqlite:///./db.sqlite"
-=======
-    API_BASE_URL = "https://api.spotify.com/v1"
->>>>>>> f9670b91c89aadfd2cdb7bf4f9c3761bee5f4230
