09aa03b77351f91e15784c13e9bd02da2cfe79b5
JoDsnr <jodesnoyer@gmail.com>
2024-02-01 15:20:10 +0100

    load spotify data in db

--- flask-backend/models.py
+++ flask-backend/models.py
@@ -11,3 +11,10 @@ class User(db.Model):
     id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid)
     email = db.Column(db.String(345), unique=True)
     password = db.Column(db.Text, nullable=False)
+
+class SpotifyHistory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    endTime = db.Column(db.String(20))
+    artistName = db.Column(db.String(100))
+    trackName = db.Column(db.String(100))
+    msPlayed = db.Column(db.Integer)
---
+++ flask-backend/scripts/spotify_history.py
@@ -0,0 +1,33 @@
+import os
+from flask import current_app
+from models import db, SpotifyHistory
+import json
+
+# Function to add Spotify history from JSON files to the database
+def add_spotify_history_from_json():
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    json_folder_path = os.path.join(script_directory, "..", "Spotify Account Data")
+
+    # Initialize Flask app context
+    app = current_app._get_current_object()
+    with app.app_context():
+        # Iterate through each JSON file in the folder
+        for file_name in os.listdir(json_folder_path):
+            if file_name.startswith("StreamingHistory_music_0"):
+                file_path = os.path.join(json_folder_path, file_name)
+
+                # Open the JSON file and load the data
+                with open(file_path, "r", encoding="utf-8") as json_file:
+                    spotify_history_data = json.load(json_file)
+
+                # Iterate through each entry in the JSON data and add it to the database
+                for entry in spotify_history_data:
+                    new_entry = SpotifyHistory(
+                        endTime=entry.get("endTime"),
+                        artistName=entry.get("artistName"),
+                        trackName=entry.get("trackName"),
+                        msPlayed=entry.get("msPlayed")
+                    )
+                    db.session.add(new_entry)
+            # Commit the changes to the database
+            db.session.commit()
